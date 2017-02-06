ff2c25dcab0b82d6dd64303d22be740ee6789714
Ahmadgit config --global user.name Ahmadgit config --global user.name Ahmad <alakmo8@gmail.com>
2017-02-06 15:54:17 -0500

    w5 d1 pair solution, Ahmad and Cersar

--- m1-w4d1-file-io-part1-exercises-pair/WordCount/Program.cs
+++ m1-w4d1-file-io-part1-exercises-pair/WordCount/Program.cs
@@ -3,13 +3,57 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using System.IO;
+using System.Text.RegularExpressions;
 namespace WordCount
 {
     class Program
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Please input the file path:");
+            string filePath = Console.ReadLine();
+            while (!File.Exists(filePath))
+            {
+                Console.WriteLine("Please input the RIGHT file path:");
+                filePath = Console.ReadLine();
+            }
+            int wordsCount = 0;
+            int sentenceCount = 0;
+            try
+            {
+                using (StreamReader fileReader = new StreamReader(filePath))
+                {
+                    while (!fileReader.EndOfStream)
+                    {
+                        string[] sentences = fileReader.ReadToEnd().Split(new char[] { '.', '!', '?' });
+
+                        sentenceCount = sentences.Length;
+                        foreach (var item in sentences)
+                        {
+                            string[] words = item.Split(' ');
+                            wordsCount += words.Length;
+                        }
+
+
+                    }
+                }
+            }
+            catch ( Exception e)
+            {
+                Console.WriteLine("Ooops! there is an Exception: "+e.Message);
+            }
+
+            Console.WriteLine(wordsCount + "                 " + sentenceCount);
+            Console.ReadKey();
+
+
+            //  C: \Users\aobiedat\Tech Elevator\.Net class\week1\c-week4-pair-exercises\m1-w4d1-file-io-part1-exercises-pair\alices_adventures_in_wonderland.txt
+            //  if (!Directory.Exists(filePath)) throw new DirectoryNotFoundException("your path is wrong");
+            //  C: \Users\aobiedat\Tech Elevator\.Net class\week1\c-week4-pair-exercises\m1-w4d1-file-io-part1-exercises-pair\testfile.txt
+
+
         }
+
     }
 }
